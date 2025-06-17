5d31095c13377f012fc4f175f82a7a448462d700
Anjali Ajith <anjusukumari@gmail.com>
2025-06-17 19:11:32 +0200

    created react scaffold instead of npm create vite@latets

--- App.js
+++ App.js
@@ -28,20 +28,17 @@
 </div>  
 
 */
-
+import React from "react";
+import ReactDOM from "react-dom";
 const parent = React.createElement("div",{id:"parent"},[ React.createElement("div",{
         id:"child1"
     }, [React.createElement("h1",{
-        key:"1"
     },"hi"),React.createElement("h2",{
-        key:"2"
     },"i am h2 tag")]),
  React.createElement("div",{
         id:"child2"
     }, [React.createElement("h1",{
-        key:"3"
     },"hi"),React.createElement("h2",{
-        key:"4"
     },"i am h2 tag")]),]
    
    
@@ -51,4 +48,3 @@ const parent = React.createElement("div",{id:"parent"},[ React.createElement("di
 // },"Hello world from react")
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-console.log(parent)
